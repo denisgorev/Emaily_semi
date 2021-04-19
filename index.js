f58388212e4055469ddbfa3458d665cc8106dc24
Denis Gorev <Denis_Gorev@epam.com>
2021-04-19 20:30:44 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express(); //express application initialiation
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+app.listen(PORT, () => console.log("server started on port 5000"));
