583ae996cbe7027959fb45f72c90d88eb9efbb68
Denis Gorev <Denis_Gorev@epam.com>
2021-04-22 21:22:56 +0300

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+
+require("./models/User");
+require("./services/passport");
+
 const app = express(); //express application initialiation
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
+});
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 app.listen(PORT, () => console.log("server started on port 5000"));
